2b0744816cb18d840e832176f593f21551f5b2d1
zuihuodeman <739002611@qq.com>
2016-04-04 14:08:43 +0800

    开始netWork

--- rxObserves/ViewController.swift
+++ rxObserves/ViewController.swift
@@ -11,14 +11,16 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-typealias dogTypes = AnimatableSectionModel<String, Dog>
+typealias dogType = AnimatableSectionModel<String, Dog>
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
-    var dogs :Array = [Dog(age: 12, name: "dagou"),Dog(age: 11, name: "xiaogou"),Dog(age: 13, name: "laogou")]
-    let sections = Variable([dogTypes]())
+    let disposeBag = DisposeBag()
     
-    let disposebag = DisposeBag()
+    let sections = Variable([dogType]())
+    
+    let dataSource = RxTableViewSectionedReloadDataSource<dogType>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,62 +30,22 @@ class ViewController: UIViewController {
         tableView.dataSource = nil
         tableView.delegate = nil
         view.addSubview(tableView)
-
         
-        
-        tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
-        
-        let dataSource = RxTableViewSectionedReloadDataSource<dogTypes>()
-        
-        dataSource.configureCell = { (ss,tableV,indexPath,element ) in
-            
-            let cell = tableV.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
-            cell.textLabel?.text = element.value.name
-            
-            return cell
-        }
-        
-        sections.asObservable()
-            .bindTo(tableView.rx_itemsWithDataSource(dataSource))
-            .addDisposableTo(disposebag)
-        
-        sections.value = [dogTypes(model: "", items: self.dogs)]
-        
-        
-        tableView.rx_modelSelected(IdentifiableValue<Dog>)
-            .subscribeNext {
-                print($0)
-            }
-            .addDisposableTo(disposebag)
+    }
     
-//        sections.asObservable()
-//            .bindTo(tableView.rx_itemsWithCellIdentifier("cell", cellType: UITableViewCell.self)) { (index, element, cell) in
-//                print(index)
-//                cell.textLabel?.text = element.name
-//                cell.detailTextLabel?.text = "\(element.age)"
-//        }.addDisposableTo(disposebag)
-        
-        
-        
-//        
-//        tableView.rx_modelSelected(Dog).subscribeNext { (dog) in
-//            let alert = UIAlertController.init(title: "message", message: dog.name, preferredStyle: UIAlertControllerStyle.Alert)
-//            alert.addAction(UIAlertAction.init(title: "ok", style: UIAlertActionStyle.Default, handler: nil))
-//            UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
-//        }.addDisposableTo(disposebag)
-        
-//        Observable.just([1,2,2,3,3])
-//            .bindTo(tableView.rx_itemsWithCellIdentifier("cell", cellType: UITableViewCell.self)) { (_, element, cell) in
-//                cell.textLabel?.text = "\(element)"
-//            }.addDisposableTo(disposebag)
-        
-        
+    private func skinTableViewDataSource(dataSource: RxTableViewSectionedReloadDataSource<dogType>){
+    
+        dataSource.configureCell = { ()
+            
+            
+        }
+    
+    
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+
+
+
+
 
