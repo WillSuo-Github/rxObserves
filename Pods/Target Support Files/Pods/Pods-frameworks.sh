e6bce42f136fc3d05629fe9b6e5165cd22ab0f14
zuihuodeman <739002611@qq.com>
2016-04-06 16:55:32 +0800

    添加第三方框架

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,11 +84,17 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/ObjectMapper.framework"
+  install_framework "Pods/RxAlamofire.framework"
   install_framework "Pods/RxCocoa.framework"
   install_framework "Pods/RxDataSources.framework"
   install_framework "Pods/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/ObjectMapper.framework"
+  install_framework "Pods/RxAlamofire.framework"
   install_framework "Pods/RxCocoa.framework"
   install_framework "Pods/RxDataSources.framework"
   install_framework "Pods/RxSwift.framework"
